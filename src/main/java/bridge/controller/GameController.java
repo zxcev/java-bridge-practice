39893bfbea36cd4ea9b857169baaab84aaafcfc0
zxcev <121966058+zxcev@users.noreply.github.com>
2023-12-05 16:17:47 +0900

    feat: `GameController` 작성

---
+++ src/main/java/bridge/controller/GameController.java
@@ -0,0 +1,61 @@
+package bridge.controller;
+
+import bridge.controller.dto.BridgeSquaresDto;
+import bridge.domain.bridge.BridgeGame;
+import bridge.domain.bridge.BridgeMaker;
+import bridge.domain.bridge.BridgeSize;
+import bridge.domain.bridge.NextStatus;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.List;
+
+public final class GameController {
+
+    private final BridgeMaker bridgeMaker;
+
+    public GameController(final BridgeMaker bridgeMaker) {
+        this.bridgeMaker = bridgeMaker;
+    }
+
+    public void run() {
+        final BridgeGame bridgeGame = createBridgeGame();
+        play(bridgeGame);
+        OutputView.printResult(bridgeGame.toBridgeGameDto());
+    }
+
+    private void play(final BridgeGame bridgeGame) {
+        playRoundRecursive(bridgeGame);
+
+        if (bridgeGame.isFailed()) {
+            final NextStatus nextStatus = InputView.inputNextGameStatus();
+
+            if (nextStatus.willReplay()) {
+                bridgeGame.retry();
+                play(bridgeGame);
+            }
+        }
+    }
+
+    private void playRoundRecursive(final BridgeGame bridgeGame) {
+        final String nextMovePosition = InputView.inputNextMovePosition();
+        bridgeGame.move(nextMovePosition);
+
+        final BridgeSquaresDto bridgeSquaresDto = bridgeGame.toBridgeSquaresDto();
+        OutputView.printSquares(bridgeSquaresDto);
+
+        if (!bridgeGame.isFailed() && !bridgeGame.isEnded()) {
+            playRoundRecursive(bridgeGame);
+        }
+    }
+
+    private BridgeGame createBridgeGame() {
+        // 1. 다리 길이 입력
+        final BridgeSize bridgeSize = InputView.inputBridgeSize();
+
+        // 2. 다리 생성
+        final List<String> bridge = bridgeMaker.makeBridge(bridgeSize.value());
+
+        // 3. 다리 게임 생성
+        return new BridgeGame(bridge);
+    }
+}
