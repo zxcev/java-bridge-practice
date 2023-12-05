8eee76f67d97c77ba72ef7e61ca301b3a70e5632
zxcev <121966058+zxcev@users.noreply.github.com>
2023-12-05 16:17:14 +0900

    feat: 다리 이동 가능한 칸에 대한 데이터 생성

--- src/main/java/bridge/domain/bridge/BridgeMaker.java
+++ src/main/java/bridge/domain/bridge/BridgeMaker.java
@@ -1,5 +1,7 @@
-package bridge;
+package bridge.domain.bridge;
 
+import bridge.domain.move.MovePosition;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,7 +11,7 @@ public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+    public BridgeMaker(final BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
@@ -17,7 +19,21 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    public List<String> makeBridge(int size) {
-        return null;
+    public List<String> makeBridge(final int size) {
+        final List<String> bridge = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            final String movePosition = generateMovePosition();
+            bridge.add(movePosition);
+        }
+
+        return bridge;
+    }
+
+
+    private String generateMovePosition() {
+        final int number = bridgeNumberGenerator.generate();
+
+        return MovePosition.from(number);
     }
 }
--- src/main/java/bridge/domain/bridge/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/domain/bridge/BridgeRandomNumberGenerator.java
@@ -1,9 +1,8 @@
-package bridge;
+package bridge.domain.bridge;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
-
     private static final int RANDOM_LOWER_INCLUSIVE = 0;
     private static final int RANDOM_UPPER_INCLUSIVE = 1;
 
