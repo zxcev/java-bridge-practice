130e3b72a5e1a9817e3c977a542754e26a4acd91
zxcev <121966058+zxcev@users.noreply.github.com>
2023-12-05 16:54:02 +0900

    rename: `GameCommand` 입력 관련 메세지 변수명 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,7 +2,7 @@ package bridge.view;
 
 import bridge.common.RetryHandler;
 import bridge.domain.bridge.BridgeSize;
-import bridge.domain.bridge.NextStatus;
+import bridge.domain.bridge.GameCommand;
 import bridge.domain.move.MovePosition;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -11,7 +11,7 @@ public final class InputView {
     private static final String START_MSG = "다리 건너기 게임을 시작합니다.";
     private static final String INPUT_BRIDGE_SIZE_MSG = "\n다리의 길이를 입력해주세요.";
     private static final String INPUT_NEXT_MOVE_POSITION_MSG = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)"; // TODO
-    private static final String INPUT_NEXT_GAME_STATUS_MSG = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String INPUT_NEXT_GAME_COMMAND_MSG = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     public static BridgeSize readBridgeSize() {
         return RetryHandler.handleRetry(InputView::_inputBridgeSize);
@@ -21,8 +21,8 @@ public final class InputView {
         return RetryHandler.handleRetry(InputView::_inputNextMovePosition);
     }
 
-    public static NextStatus readGameCommand() {
-        return RetryHandler.handleRetry(InputView::_inputNextGameStatus);
+    public static GameCommand readGameCommand() {
+        return RetryHandler.handleRetry(InputView::_inputNextGameCommand);
     }
 
     private static String _inputNextMovePosition() {
@@ -31,10 +31,10 @@ public final class InputView {
         return MovePosition.from(input);
     }
 
-    private static NextStatus _inputNextGameStatus() {
-        System.out.println(INPUT_NEXT_GAME_STATUS_MSG);
+    private static GameCommand _inputNextGameCommand() {
+        System.out.println(INPUT_NEXT_GAME_COMMAND_MSG);
         final String input = readLine();
-        return NextStatus.from(input);
+        return GameCommand.from(input);
     }
 
 
